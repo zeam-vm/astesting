f47e8232f13f9cb64bd754b7080ee9889f991db2
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-10 01:03:35 +0900

    fix

--- lib/mix/tasks/test/astesting/astesting.ex
+++ lib/mix/tasks/test/astesting/astesting.ex
@@ -35,13 +35,12 @@ defmodule Mix.Tasks.Test.Astesting do
           {_, 1} -> :ok
 
           _ ->
-            IO.puts(Mix.Project.app_path())
-            case File.read("#{Mix.Project.app_path()}/priv/Dockerfile.template") do
+            case File.read("#{Map.get(Mix.Project.apps_paths(), :astesting)}/priv/Dockerfile.template") do
               {:error, :enoent} -> IO.puts("Dockerfile.template is lost.")
               {:error, reason} -> IO.puts(:file.format_error(reason))
               {:ok, binary} ->
                 binary = String.replace(binary, "@version", "#{System.version()}-alpine", global: false)
-                dockerfile = "#{Mix.Project.app_path()}/priv/Dockerfile"
+                dockerfile = "#{Map.get(Mix.Project.apps_paths(), :astesting)}/priv/Dockerfile"
                 case File.write(dockerfile, binary) do
                   {:error, reason} -> IO.puts(:file.format_error(reason))
                   :ok ->
